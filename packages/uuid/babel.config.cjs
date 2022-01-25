e5c92e030b192540ecf3bf4dbd08feb8f6980718
Harshal Patil <harshal.rp@gmail.com>
2022-01-25 11:43:11 +0530

    Monorepo setup

---
+++ packages/uuid/babel.config.cjs
@@ -0,0 +1,5 @@
+const config = require('../../config/babel.config.cjs');
+
+module.exports = {
+  ...config
+};
---
+++ packages/uuid/jest.config.js
@@ -0,0 +1,9 @@
+import base from '../../config/jest.config.js';
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  ...base
+};
